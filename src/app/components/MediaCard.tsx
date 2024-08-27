c19e04f2f1b0200623766b6b52e354b50d73b802
Megan Chantosa <meganemmamoore@gmail.com>
2024-08-27 02:20:22 -0700

    fixed the card not centering in small window issue

--- src/app/components/MediaCard.tsx
+++ src/app/components/MediaCard.tsx
@@ -6,6 +6,7 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
+import { Grid, Box } from "@mui/material";
 
 export default function MediaCard({
   title,
@@ -16,34 +17,43 @@ export default function MediaCard({
 }) {
   return (
     <Link href={_href}>
-      <Card
+      <Box
         sx={{
-          width: 325,
-          minWidth: 325,
           display: "flex",
-          flexDirection: "column",
-          cursor: "pointer", // Change cursor to pointer to indicate it's clickable
-          transition: "transform 0.2s ease, box-shadow 0.2s ease", // Smooth transition for hover effects
-          "&:hover": {
-            transform: "scale(1.05)", // Slightly scale up the card on hover
-            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)", // Add shadow to create depth
-          },
-          "&:focus": {
-            outline: "2px solid #003366", // Add a border on focus for accessibility
-          },
-          height: "100%", // Ensure card takes full height of container
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
         }}
       >
-        <CardMedia sx={{ height: 180 }} image={media} title={mediaTitle} />
-        <CardContent sx={{ flexGrow: 1 }}>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {description}
-          </Typography>
-        </CardContent>
-      </Card>
+        <Card
+          sx={{
+            width: 325,
+            minWidth: 325,
+            display: "flex",
+            flexDirection: "column",
+            cursor: "pointer", // Change cursor to pointer to indicate it's clickable
+            transition: "transform 0.2s ease, box-shadow 0.2s ease", // Smooth transition for hover effects
+            "&:hover": {
+              transform: "scale(1.05)", // Slightly scale up the card on hover
+              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)", // Add shadow to create depth
+            },
+            "&:focus": {
+              outline: "2px solid #003366", // Add a border on focus for accessibility
+            },
+            height: "100%", // Ensure card takes full height of container
+          }}
+        >
+          <CardMedia sx={{ height: 180 }} image={media} title={mediaTitle} />
+          <CardContent sx={{ flexGrow: 1 }}>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {description}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
     </Link>
   );
 }
